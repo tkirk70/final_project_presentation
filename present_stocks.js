2daeac87ae00d3ffc225bae79144b8276d799efa
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-04-27 14:45:32 -0400

    Interactive Ticker Graphs.

--- present_stocks.js
+++ present_stocks.js
@@ -1,68 +1,39 @@
-// Display stock financials and graphs for 9 stocks in interactive webpage.
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+function update() {
+  var select = document.getElementById('myList');
+  var option = select.options[select.selectedIndex];
+  var value = select.options[select.selectedIndex].value;
+  var text = select.options[select.selectedIndex].text;
+  document.getElementById('value').value = option.value;
+  console.log(value); // en
+	document.getElementById('text').value = option.text;
+  console.log(text); // en
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  d3.csv(`https://raw.githubusercontent.com/tkirk70/final_project_presentation/main/stockprice_daily_${value}.csv`).then((data) => {
+    var sampleNames = data[0].ticker;
+    var y = [];
+    var x = [];
+    for (var i = 0; i < data.length; i++) { x.push(data[i].date);
+    y.push(data[i].close) };
+
+    var trace1 = {
+      x: x,
+      y: y,
+      type: 'line'
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: `Decade of stock values for ${sampleNames}`,
+      xaxis: {
+        title: 'Year'
+      },
+      yaxis : {
+        title: 'Stock Value in US Dollars ($)'
+        }
       }
-    }
+
+    Plotly.newPlot('line', data, layout);
+  })
   }
-}
-
-// d3.csv("stockprice_daily_aapl.csv", function(data) {
-//     for (var i = 0; i < data.length; i++) {
-//         console.log(data[i].date);
-//         console.log(data[i].close);
-//     }
-// });
-//
-// d3.csv("stockprice_daily_aapl.csv")
-//     .row(function(d) { return {key: d.key, value: +d.value}; })
-//     .get(function(error, rows) { console.log(rows); });
-
-
-
-
-function makeplot() {
-  d3.csv("stockprice_daily_aapl.csv", function(data){ processData(data) } );
-
-};
-
-function processData(allRows) {
-
-  // console.log(allRows);
-  var x = [], y = [], standard_deviation = [];
-  // console.log(allRows['date'])
-
-  for (var i=0; i<allRows.length; i++) {
-    console.log(allRows['date'])
-    // row = allRows[i];
-    // console.log(row);
-    x.push( allRows['date'] );
-    y.push( allRows['close'] );
-  }
-  console.log( 'X',x, 'Y',y, 'SD',standard_deviation );
-  makePlotly( x, y, standard_deviation );
-}
-
-function makePlotly( x, y, standard_deviation ){
-  var plotDiv = document.getElementById("plot");
-  var traces = [{
-    x: x,
-    y: y
-  }];
-
-  Plotly.newPlot('line', traces,
-    {title: 'Plotting CSV data from AAPL call'});
-};
-  makeplot();
+update();
