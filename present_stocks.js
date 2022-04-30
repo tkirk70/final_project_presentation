1ca652a3f37aff0abee37f467221b461534d72f3
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-04-30 14:35:24 -0400

    Fixing csv files.

--- present_stocks.js
+++ present_stocks.js
@@ -74,8 +74,10 @@ var url = `box_o_files/${value}.csv`;
 
       var layout = {
         title: `Real versus Predicted Returns ${value}`,
+        font: {size: 23},
         xaxis: {
           title: 'Year',
+          font: {size: 19},
           tickmode: "linear", //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
           tick0: '11/6/2019',
           dtick: 30 //* 24 * 60 * 60 * 1000 // milliseconds
