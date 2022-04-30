95786e96cb6f7dce44ff6a9b09cc53bed28fbbda
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-04-30 15:19:36 -0400

    Fixing csv files.

--- present_stocks.js
+++ present_stocks.js
@@ -84,7 +84,7 @@ var url = `box_o_files/${value}.csv`;
           tickmode: "linear", //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
           tick0: '11/6/2019',
           dtick: 30 //* 24 * 60 * 60 * 1000 // milliseconds
-        },
+        ,
         yaxis: {
           title: 'Stock Value in US Dollars ($)'
         }
